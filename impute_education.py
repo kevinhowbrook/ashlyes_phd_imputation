d10a03116029c19aa8332704318beb981aa8b5c7
kevinhowbrook <kbhowbrook@gmail.com>
2019-03-14 00:33:01 +0000

    More logic comments'

--- impute_education.py
+++ impute_education.py
@@ -58,9 +58,9 @@ df = load_large_dta(data_file)
 # Find a pidp with grades - 2051
 # for i, row in enumerate(df.itertuples()):  # enumeration means the row begins 0
 #     if type(row[3]) == str and row[3] != 'no qual':
-#         print(row[1])
-#         exit()
+#         print(row[3])
 
+# exit()
 
 #df = df.head(78)
 new_df = pd.DataFrame()
@@ -98,6 +98,44 @@ for i, row in enumerate(df.itertuples()):  # enumeration means the row begins 0
     for _i, _row in tmp_data.iterrows():
         pass
         #tmp_data.at[_i,'high_qual'] = 'Overridden'
+        # First row will start at age 16
+
+        # Assumptions
+        # Average age for qualifications
+        # - no_qual is the default and will replace NaN, it means no_qual
+        #   Someone with any qualification will never get no_qual assigned.
+        # - GCSE 16
+        # - A-Level 18
+        # - other higher qual = 20
+
+        """ EG
+        If we find someone has a first_degree value at age 50,
+        this means at 50 they were observed with a degree.
+        We assume that the age they got that degree would be 21
+        BUT, if we creep back in the data and find they have high_qual A-levels
+        at age 48, then we know they got there degree at 49.
+        so:
+        45 NaN
+        46 NaN
+        47 NaN
+        48 a_levels
+        49 first_degree
+        49 first_degree
+
+        Given the above, we now need to add valuse going back from alevls to be like
+        24 NaN
+        25 NaN
+        26 GCSE    << Note GCSE pops up, se we fill in 25,24 etc going back to age 16.
+        ...
+        46 a_levels
+        47 a_levels
+        48 a_levels
+        49 first_degree
+        49 first_degree
+
+        """
+
+
         # logic here for amending/overriding values
         # in pidp 2051, the high_qual is first_degree, age achieved 21.
         # So we need to fill in high_qual values going back to when they were 21 or
